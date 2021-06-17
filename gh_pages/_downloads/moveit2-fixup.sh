ad333a2805be44292ca9166abfe9d3d2c64c1521
ben <ben.greenberg@swri.org>
2021-06-17 11:23:16 -0500

    go to root of ws

--- gh_pages/_downloads/moveit2-fixup.sh
+++ gh_pages/_downloads/moveit2-fixup.sh
@@ -12,5 +12,6 @@ git checkout 2c3aad1
 cd ~/moveit2/src/ros2_controllers
 git checkout 8587e07
 
+cd ~/moveit2/
 source /opt/ros/foxy/setup.sh
 colcon build --merge-install --parallel-workers 2 --cmake-args -DCMAKE_BUILD_TYPE=RelWithDebInfo
